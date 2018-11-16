fa4b32d7ae88ea105a33427e8fd4987c432c35af
ctapang <ctapang@hotmail.com>
2018-11-15 16:30:17 -0800

    Upgrade to VS 2017, Boost 1.68.0

--- include/caffe/layers/pooling_layer.hpp
+++ include/caffe/layers/pooling_layer.hpp
@@ -9,110 +9,75 @@
 
 namespace caffe {
 
-  /**
-  * @brief Pools the input image by taking the max, average, etc. within regions.
-  *
-  * TODO(dox): thorough documentation for Forward, Backward, and proto params.
-  */
-
-#define PARTSNUM  9
-
-  template <typename Dtype>
-  class PoolingLayer : public Layer<Dtype> {
-  public:
-    explicit PoolingLayer(const LayerParameter& param)
+/**
+ * @brief Pools the input image by taking the max, average, etc. within regions.
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+template <typename Dtype>
+class PoolingLayer : public Layer<Dtype> {
+ public:
+  explicit PoolingLayer(const LayerParameter& param)
       : Layer<Dtype>(param) {}
-    virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-                            const vector<Blob<Dtype>*>& top);
-    virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-                         const vector<Blob<Dtype>*>& top);
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
 
-    virtual inline const char* type() const { return "Pooling"; }
-    virtual inline int ExactNumBottomBlobs() const { return 1; }
-    virtual inline int MinTopBlobs() const { return 1; }
-    // MAX POOL layers can output an extra top blob for the mask;
-    // others can only output the pooled inputs.
-    virtual inline int MaxTopBlobs() const {
-      return (this->layer_param_.pooling_param().pool() ==
-              PoolingParameter_PoolMethod_MAX) ? 2 : 1;
-    }
+  virtual inline const char* type() const { return "Pooling"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int MinTopBlobs() const { return 1; }
+  // MAX POOL layers can output an extra top blob for the mask;
+  // others can only output the pooled inputs.
+  virtual inline int MaxTopBlobs() const {
+    return (this->layer_param_.pooling_param().pool() ==
+            PoolingParameter_PoolMethod_MAX) ? 2 : 1;
+  }
 
-  protected:
-    virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-                             const vector<Blob<Dtype>*>& top);
-    virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-                             const vector<Blob<Dtype>*>& top);
-    virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-                              const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-                              const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
-    int kernel_h_, kernel_w_;
-    int stride_h_, stride_w_;
-    int pad_h_, pad_w_;
-    int channels_;
-    int height_, width_;
-    int pooled_height_, pooled_width_;
-    bool global_pooling_;
-    Blob<Dtype> rand_idx_;
-    Blob<int> max_idx_;
+  int kernel_h_, kernel_w_;
+  int stride_h_, stride_w_;
+  int pad_h_, pad_w_;
+  int channels_;
+  int height_, width_;
+  int pooled_height_, pooled_width_;
+  bool global_pooling_;
+  Blob<Dtype> rand_idx_;
+  Blob<int> max_idx_;
+};
 
-    // parameters for deformation layer
-    virtual void dt(int dims0, int dims1,
-                    const Dtype * vals, Dtype ax, Dtype bx, Dtype ay, Dtype by, int n, int ch);
+template <typename Dtype>
+__global__ void MaxPoolBackward(const int nthreads, const Dtype* const top_diff,
+	const int* const mask, const Dtype* const top_mask, const int num,
+	const int channels, const int height, const int width,
+	const int pooled_height, const int pooled_width, const int kernel_h,
+	const int kernel_w, const int stride_h, const int stride_w, const int pad_h,
+	const int pad_w, Dtype* const bottom_diff);
 
-    virtual void reduce1dtran(const Dtype *src, int sheight, Dtype *dst, int dheight,
-                              int width, int chan);
+template <typename Dtype>
+__global__ void AvePoolBackward(const int nthreads, const Dtype* const top_diff,
+	const int num, const int channels, const int height,
+	const int width, const int pooled_height, const int pooled_width,
+	const int kernel_h, const int kernel_w, const int stride_h,
+	const int stride_w, const int pad_h, const int pad_w,
+	Dtype* const bottom_diff);
 
-    virtual void reduce1dtran(Dtype *src, int sheight, Dtype *dst, int dheight,
-                              int width, int chan);
-
-    int N_;
-    int Flag_;
-    int parts_num_;
-    int hpos_[PARTSNUM];
-    int vpos_[PARTSNUM];
-    double blobl_a_min_;
-    /*  vector<Dtype> defw1_;
-    vector<Dtype> defw2_;
-    vector<Dtype> defw3_;
-    vector<Dtype> defw4_;
-    vector<Dtype> d_defw1_;
-    vector<Dtype> d_defw2_;
-    vector<Dtype> d_defw3_;
-    vector<Dtype> d_defw4_;*/
-
-    vector<int> defh_;
-    vector<int> defv_;
-    vector<int> defp_;
-    vector<int>Ih_;
-    vector<int>Iv_;
-    vector<int>tmpIx_;
-    vector<int>tmpIy_;
-    vector<Dtype>Mdt_;
-    vector<Dtype>tmpM_;
-    /*
-    shared_ptr<SyncedMemory> defh_;
-    shared_ptr<SyncedMemory> defv_;
-    shared_ptr<SyncedMemory> defp_;
-    shared_ptr<SyncedMemory>Ih_;
-    shared_ptr<SyncedMemory>Iv_;
-    shared_ptr<SyncedMemory>tmpIx_;
-    shared_ptr<SyncedMemory>tmpIy_;
-    shared_ptr<SyncedMemory>Mdt_;
-    shared_ptr<SyncedMemory>tmpM_;
-    */
-
-    //  Blob<Dtype> col_buffer_;
-    Blob<Dtype> top_buffer_;
-    Blob<Dtype> dh_;
-    Blob<Dtype> dv_;
-    Blob<Dtype> dh2_;
-    Blob<Dtype> dv2_;
-    int kernel_size_;
-    int stride_;
-    int pad_;
-  };
+template <typename Dtype>
+__global__ void StoPoolBackward(const int nthreads,
+	const Dtype* const rand_idx, const Dtype* const top_diff,
+	const int num, const int channels, const int height,
+	const int width, const int pooled_height, const int pooled_width,
+	const int kernel_h, const int kernel_w, const int stride_h,
+	const int stride_w, Dtype* const bottom_diff);
 
 }  // namespace caffe
 
